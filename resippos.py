1055322ac9acb4353ca367ec5f7b8021c792e4a3
Juuso Selin <juuso.selin@gmail.com>
2016-11-02 10:02:56 +0200

    Initial commit

---
+++ resippos.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+'''
+Resippos.py: Really Simple Python Port Scanner
+'''
+
+# Imports
+import sys
+
+# Functions
+
+
+# Main function
+def main():
+    '''Main function description'''
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
