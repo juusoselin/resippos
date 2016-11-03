c54762259697586d54a9ca3a2242807fb3370225
Juuso Selin <juuso.selin@gmail.com>
2016-11-03 17:53:38 +0200

    Add main function

--- resippos.py
+++ resippos.py
@@ -12,19 +12,23 @@ from datetime import datetime
 
 
 # Functions
-def define_input():
+def define_host():
     ''' Ask user a host name and get that hosts IP address'''
-    remote = raw_input('Enter host: ')
+    remote = input('Enter host: ')
     if not remote[0].isalnum() or not '.' in remote:                    # Quick check of user input
         print('Your input is not valid')
         exit()
     remoteIP = socket.gethostbyname(remote)                             # Get host IP
-    ports = raw_input('Which ports do we scan? ')
 
+    return remoteIP
+
+def define_ports():
+    ''' Ask ports to be scanned'''
+    ports = input('Which ports do we scan? ')
     if ports.lower() == 'a':                                            # All ports (1 - 1024)
         ports = range(1, 1025)
     elif ports.isdigit():                                               # Single port
-        port = ports
+        ports = [ports]
     elif '-' in ports:                                                  # Range of ports
         splitted = ports.split('-')
         ports = range(int(splitted[0]), int(splitted[1]) + 1)
@@ -32,16 +36,24 @@ def define_input():
         print('Your input is not valid')
         exit()
 
-    print(ports)
-    return {remote: remoteIP}
+    return ports
+
 
 # Main function
 def main():
     '''Main function description'''
     subprocess.call('clear', shell=True)                                # Clear the screen
 
-    print(define_input())
-    pass
+    host = define_host()
+    ports = define_ports()
+
+    for port in ports:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if s.connect_ex((host, int(port))) == 0:
+            print('Port', port, ' is open')
+            s.close()
+    #print(host)
+    #print(ports)
 
 if __name__ == '__main__':
     sys.exit(main())
